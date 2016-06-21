84241b0c833e52d33d10962d5e67f752c3467afa
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 09:30:14 +0300

    Домашнее задание 2 Последовательность Фибоначчи

--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Мы сейчас посчитаем сумму 11 первых членов последовательности Фибеначчи");
+        System.out.println("Мы сейчас посчитаем 11 первых членов последовательности Фибеначчи");
         int[] fibonachiList = new int[11];
         fibonachiList[0] = 1;
         fibonachiList[1] = 1;
