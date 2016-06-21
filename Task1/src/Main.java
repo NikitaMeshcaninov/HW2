ad71e42676207de90eb56d5503a0aea9dd2f7dc4
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 09:21:30 +0300

    Задание 1 сумма чисел

---
+++ Task1/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Введите число");
+        Scanner sc = new Scanner(System.in);
+        String number = sc.next();
+        int Summ = 0;
+        char [] numberArray = new char[number.length()];
+        for (int i=0;i<number.length();i++){
+            Summ += number.charAt(i)-48;
+        }
+        System.out.println("Сумма чисел вашего числа составляет = " + Summ);
+    }
+}
