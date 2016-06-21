0f96e926f99547f368e582edf0984401386a1413
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 09:24:51 +0300

    Задание 2 последовательность Фибоначчи

---
+++ Task2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Мы сейчас посчитаем сумму 11 первых членов последовательности Фибеначчи");
+        int[] fibonachiList = new int[11];
+        fibonachiList[0] = 1;
+        fibonachiList[1] = 1;
+        int summFibonachiList = 2;
+        for (int i = 2; i < 11; i++) {
+            fibonachiList[i] = fibonachiList[i - 1] + fibonachiList[i - 2];
+            System.out.println(fibonachiList[i]);
+        }
+    }
+}
