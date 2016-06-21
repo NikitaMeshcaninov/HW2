8c67ac76f2acff9ea40b03ae3a48bd2de9883e6d
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 09:32:15 +0300

    Домашнее задание 2 Суммацифр числа

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -7,9 +7,9 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String number = sc.next();
         int Summ = 0;
-        char [] numberArray = new char[number.length()];
-        for (int i=0;i<number.length();i++){
-            Summ += number.charAt(i)-48;
+        char[] numberArray = new char[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            Summ += number.charAt(i) - 48;
         }
         System.out.println("Сумма чисел вашего числа составляет = " + Summ);
     }
