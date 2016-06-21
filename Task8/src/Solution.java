a4318d66b09d1ed511c10a5119caa3da5b95595a
NIkita Meshchaninov <meshn@outlook.com>
2016-06-21 15:15:21 +0300

    Домашнее задание 2 задача 8 посчитать сколько раз нужно вычеслить 4й член последоватеьности ряда Фибоначчи для вычисления 15го

---
+++ Task8/src/Solution.java
@@ -0,0 +1,17 @@
+public class Solution {
+static  int counter = 0;
+
+    public static int fib(int i) {
+        if (i == 1) return 1;
+        if (i == 2) return 1;
+        if (i==4) counter++;
+        return fib(i - 1) + fib(i - 2);
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println("15 элемент последоватеньности Фибоначчи " + fib(15));
+        System.out.println("для вычисления 15го элемента ряда 4й элемент будет высчитан " + counter+" раз");
+    }
+}
